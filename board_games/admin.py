ba8304aa31fc70b40d88880f0b03210efb660f46
MasterOfComedy985 <tikka.tuomas@gmail.com>
2021-12-09 16:51:49 +0200

    basic admin site and models

--- board_games/admin.py
+++ board_games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from board_games.models import Game, Description
 
 # Register your models here.
+
+
+admin.site.register(Game)
+admin.site.register(Description)
--- board_games/models.py
+++ board_games/models.py
@@ -1,3 +1,25 @@
+from django.core.exceptions import DisallowedHost
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    """A board game"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class Description(models.Model):
+    """Description of a board game"""
+    game = models.ForeignKey(Game, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'descriptions'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
